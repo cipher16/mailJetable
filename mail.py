4aa1c37afb5b4d1cedb24936883a8821c6c80011
cipher16 <cipher16@cipher16.(none)>
2010-06-29 20:10:10 +0200

    Ajout d'une tache cron de nettoyage

--- mail.py
+++ mail.py
@@ -7,12 +7,14 @@ from Controllers.DisplayContent import DisplayContent
 from Controllers.CreateMail import CreateMail
 from Controllers.DisplayMails import DisplayMails
 from Controllers.LogSenderHandler import LogSenderHandler
+from Controllers.CleanCronTask import CleanCronTask
 
 application = webapp.WSGIApplication(
 	[('/', MainPage),
 	('/createMail', CreateMail),
 	('/displayMails', DisplayMails),
 	('/displayContent', DisplayContent),
+	('/clean', CleanCronTask),
 	(LogSenderHandler.mapping())],
 	debug=True)
 
